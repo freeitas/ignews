c356da8e66ee2a53b575b3aaa7db12e1efa38a1a
Guilherme Freitas <hfdsguilherme@gmail.com>
2021-03-24 20:45:48 -0300

    chore: components e paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
+
 export default function Home() {
   return (
-    <div >
-      <h1>hello</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about the <span>React</span>world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   )
 }
